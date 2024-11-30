15af9a7fb72c6e97a46cec2ab09629ab0b5cd95e
saicharan.thirandas <saicharan.thirandas@medtronic.com>
2024-11-29 21:01:13 -0500

    train mimic cxr script

---
+++ scripts/train/train_mimic.sh
@@ -0,0 +1,14 @@
+FINETUNE_DATA_PATH="/data/annotations/mimic-cxr-jpg/mimic_reports_all_images.json" #finetune annotation file path
+FINETUNE_IMAGE_PATH="/data/mount/mimic-cxr-jpg-2.1.0.physionet.org" #finetune image dir
+
+LLM_VERSION=microsoft/phi-2 # llm path in huggingface
+VT_VERSION=google/siglip-so400m-patch14-384 #vision tower path in huggingface
+VT_VERSION2="" #if you are not using mof vision tower, keep it empty
+CN_VERSION=mlp2x_gelu #connector type, other options are: qformer, resampler, etc
+CONV_VERSION=phi #chat template, other options are: phi, llama, gemmma, etc
+VERSION=base #experiment name for recording different runnings
+TRAIN_RECIPE=common #training recipes, other options are: lora, qlora
+MODEL_MAX_LENGTH=3072 #max model length for llm
+
+
+bash scripts/train/finetune.sh "$FINETUNE_DATA_PATH" "$FINETUNE_IMAGE_PATH" "$LLM_VERSION" "$VT_VERSION" "$VT_VERSION2" "$CN_VERSION" "$CONV_VERSION" "$VERSION" "$TRAIN_RECIPE" "$MODEL_MAX_LENGTH"
