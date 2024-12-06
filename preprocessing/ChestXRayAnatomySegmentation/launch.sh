40b6ad58d33952ce93c97cd98e5693a001281730
Saicharan Thirandas <saicharan.thirandas@medtronic.com>
2024-12-05 20:44:52 -0500

    launch inference

---
+++ preprocessing/ChestXRayAnatomySegmentation/launch.sh
@@ -0,0 +1,12 @@
+# First 3 splits on GPU 0
+for num in {1..3}; do
+  CUDA_VISIBLE_DEVICES=0 nohup python3 bin/cxas_segment_bulk_file_input.py -i /home/thiras3/workspace/TinyLLaVA-CXR/annotations/mimic-cxr-jpg/split_image_files/image_filepaths_split_${num}.txt > nohup_${num} 2>&1 &
+  sleep 10
+done
+
+# Next 3 splits on GPU 1
+for num in {4..6}; do
+  CUDA_VISIBLE_DEVICES=1 nohup python3 bin/cxas_segment_bulk_file_input.py -i /home/thiras3/workspace/TinyLLaVA-CXR/annotations/mimic-cxr-jpg/split_image_files/image_filepaths_split_${num}.txt > nohup_${num} 2>&1 &
+  sleep 10
+done
+
